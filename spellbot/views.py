12d9d1ba8da22cff3e5aa5b12b9823c0cb2e8962
Donchess1 <stepabod@yahoo.com>
2025-02-22 19:45:40 +0100

    install bs4

--- spellbot/views.py
+++ spellbot/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 class HangmanGameView(APIView):
     def post(self, request):
         """Processes Hangman game commands and letter guesses."""
-        data = request.json
+        data = request.data
         
         channel_id = "019524fa-e7e9-73f9-9b96-04432d261992"
         content_html= data.get("content", "")
