2bf6caea241a2b51f2d27da6a652ce10b7f63a30
Donchess1 <Stepabod@yahoo.com>
2025-02-23 20:21:41 +0100

    Update channel

--- spellbot/views.py
+++ spellbot/views.py
@@ -8,7 +8,7 @@ class HangmanGameView(APIView):
     def post(self, request):
         """Processes Hangman game commands and letter guesses."""
         data = request.data
-        channel_id = 019524fa-e7e9-73f9-9b96-04432d261992
+        channel_id = data.user_id
         messages = data.get("message", "")
         soup = BeautifulSoup(messages, "html.parser")
         user_input = soup.get_text().strip().lower()if messages else ""
