3dd2023833dd755386e229a63a59d84fae09a785
Donchess1 <stepabod@yahoo.com>
2025-02-24 19:21:47 +0100

    updated settings CORS

--- Telex_spellbot/settings.py
+++ Telex_spellbot/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'spellbot',
+    "corsheaders",
     'rest_framework',
 ]
 
@@ -43,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'Telex_spellbot.urls'
