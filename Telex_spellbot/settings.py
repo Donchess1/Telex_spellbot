2c02a9eb16983e996b5974c2dba438edd3223726
Donchess1 <Stepabod@yahoo.com>
2025-02-24 19:06:07 +0100

    Added telex cor

--- Telex_spellbot/settings.py
+++ Telex_spellbot/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-au&c074gdyquido4(3j6pe-g_q#p3*c(__s9r8^j5^g9s-c4&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["telex-spellbot.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["telex-spellbot.onrender.com", "localhost", "127.0.0.1", "telex.im"]
 
 
 # Application definition
